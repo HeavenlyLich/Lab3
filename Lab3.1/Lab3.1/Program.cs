2b1fe7801519a09165d6cd398cf92a10a69aea06
HeavenlyLich <130435636+HeavenlyLich@users.noreply.github.com>
2023-10-04 15:06:27 +0300

    Add files via upload

---
+++ Lab3.1/Lab3.1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ind
+{
+    class Dogs
+    {
+        public static List<Dog> DogsList = new List<Dog>();
+        public static void Add(Dog d)
+        {
+            DogsList.Add(d);
+        }
+        public static void Show()
+        {
+            foreach (Dog d in DogsList)
+            {
+                d.PrintToConsole();
+            }
+        }
+    }
+
+    class Dog
+    {
+        public string Name { get; private set; }
+        public int Weight { get; private set; }
+        public string Color { get; private set; }
+
+        public Dog(string name, int weight, string color)
+        {
+            Name = name;
+            Weight = weight;
+            Color = color;
+        }
+
+        public void PrintToConsole()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Ім'я собаки:{Name}");
+            Console.WriteLine($"Вага собаки:{Weight} кг");
+            Console.WriteLine($"Колір собаки:{Color}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(@"D:\visual\XMLFile2.xml");
+            foreach (XmlNode node in xml.DocumentElement)
+            {
+                string name = node["dogName"].InnerText;
+                int weight = Int32.Parse(node["dogWeight"].InnerText);
+                string color = node["dogColor"].InnerText;
+
+                Dogs.Add(new Dog(name, weight, color));
+            }
+            Dogs.Show();
+        }
+    }
+}
---
+++ Lab3.2/Lab3.2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ind
+{
+    class Realty
+    {
+        public static List<Location> LocationsList = new List<Location>();
+        public static void Add(Location l)
+        {
+            LocationsList.Add(l);
+        }
+        public static void Show()
+        {
+            foreach (Location l in LocationsList)
+            {
+                l.PrintToConsole();
+            }
+        }
+    }
+
+    class Location
+    {
+        public string Country { get; private set; }
+        public string Region { get; private set; }
+        public string District { get; private set; }
+        public string LocalityName { get; private set; }
+        public string SubLocalityName { get; private set; }
+        public string NonAdminSubLocality { get; private set; }
+        public string Address { get; private set; }
+        public string Direction { get; private set; }
+
+        public Location(string country, string region, string district, string localityName,
+                        string subLocalityName, string nonAdminSubLocality,
+                        string address, string direction)
+        {
+            Country = country;
+            Region = region;
+            District = district;
+            LocalityName = localityName;
+            SubLocalityName = subLocalityName;
+            NonAdminSubLocality = nonAdminSubLocality;
+            Address = address;
+            Direction = direction;
+        }
+
+        public void PrintToConsole()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Країна:{Country}");
+            Console.WriteLine($"Регіон:{Region}");
+            Console.WriteLine($"Район:{District}");
+            Console.WriteLine($"Назва населеного пункту:{LocalityName}");
+            Console.WriteLine($"Назва підлокальності:{SubLocalityName}");
+            Console.WriteLine($"Назва неадміністративної підлокальності:{NonAdminSubLocality}");
+            Console.WriteLine($"Адреса:{Address}");
+            Console.WriteLine($"Напрямок:{Direction}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(@"D:\visual\XMLFile3.xml");
+
+            foreach (XmlNode node in xml.DocumentElement)
+            {
+                string country = node["country"].InnerText;
+                string region = node["region"].InnerText;
+                string district = node["district"].InnerText;
+                string localityName = node["locality-name"].InnerText;
+                string subLocalityName = node["sub-locality-name"] != null ? node["sub-locality-name"].InnerText : "";
+                string nonAdminSubLocality = node["non-admin-sub-locality"] != null ? node["non-admin-sub-locality"].InnerText : "";
+                string address = node["address"] != null ? node["address"].InnerText : "";
+                string direction = node["direction"] != null ? node["direction"].InnerText : "";
+
+                Realty.Add(new Location(country, region, district, localityName,
+                                        subLocalityName, nonAdminSubLocality,
+                                        address, direction));
+            }
+
+            Realty.Show();
+        }
+    }
+}
